7cdc0b9733b2a76be13edb6d3be5aeeb1a886062
kongnakorn <kongnakornna@gmail.com>
2021-07-17 12:53:24 +0700

    sql update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import app from './app'
 const port =  process.env.PORT || 8001
 const address = '0.0.0.0'  
-/**************************/
+/******************************/
+/***********redis**************/
+const redis = require('redis').createClient({ host:  process.env.redis_host || 'localhost', port: process.env.redis_port || 6379 })
+app.register(require('fastify-redis'), {
+    host: process.env.redis_host || '127.0.0.1',
+    port: process.env.redis_port || 6380,
+    namespace: 'hello redis'
+  })
+app.register(require('fastify-redis'), {
+    client: redis,
+    namespace: 'test redis'
+})
+/***********redis**************/
 const start = async () => {
   try {
     await app.listen(port, address)
-    console.log('NodeJs Fastify V.1.0.1 knex server listening on ' + address +':'+ port)
+      console.log('NodeJs Fastify V.1.0.1 knex server listening on ' + address + ':' + port)
+      
+       console.log(redis)
   } catch (error) {
     console.log(error)
     process.exit(0)
