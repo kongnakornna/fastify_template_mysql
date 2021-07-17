d8aae6d8ed2a484d082bd6e7d390c5a9245e8800
kongnakorn <kongnakornna@gmail.com>
2021-07-17 18:34:52 +0700

    up

--- src/modules/upload/controllers/upload.ts
+++ src/modules/upload/controllers/upload.ts
@@ -100,13 +100,13 @@ export default async function upload(fastify: FastifyInstance) {
     if (params=='') {
             reply.code(500).send({ status: false,code: 500,message: 'params is null',message_th: 'ไม่พบข้อมูล params' })
             console.log(request.body)
-            return // reply.sent = true  //exit()  to stop the function execution   ออกจากลูปการทำงาน 
+            return // reply.sent = true  //exit(); to stop the function execution   ออกจากลูปการทำงาน 
         }
     const fileId = params.fileId
     if (fileId=='') {
             reply.code(500).send({ status: false,code: 500,message: 'file_id is null',message_th: 'ไม่พบข้อมูล file_id' })
             console.log(request.body)
-            return // reply.sent = true  //exit()  to stop the function execution   ออกจากลูปการทำงาน 
+            return // reply.sent = true  //exit(); to stop the function execution   ออกจากลูปการทำงาน 
         } 
 
     try {
@@ -128,7 +128,7 @@ export default async function upload(fastify: FastifyInstance) {
       } else {
         reply.code(500).send({ status: false,code: 500,message: 'file_id is null in database',message_th: 'ไม่พบข้อมูล file_id ใน database', error: 'File not found (database)' })
             console.log(request.body)
-            return // reply.sent = true  //exit()  to stop the function execution   ออกจากลูปการทำงาน  
+            return // reply.sent = true  //exit(); to stop the function execution   ออกจากลูปการทำงาน  
       }
 
     } catch (error) {
@@ -146,7 +146,7 @@ export default async function upload(fastify: FastifyInstance) {
         if (dir=='') {
                 reply.code(500).send({ status: false,code: 500,message: 'dir is null',message_th: 'ไม่พบข้อมูล dir' })
                 console.log(request.body)
-                return // reply.sent = true  //exit()  to stop the function execution   ออกจากลูปการทำงาน
+                return // reply.sent = true  //exit(); to stop the function execution   ออกจากลูปการทำงาน
             } 
   
     }
